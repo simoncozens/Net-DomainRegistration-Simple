38cd62391eb1216d78ace13f88e75cf432778e64
Simon Cozens <simon@simon-cozens.org>
2010-07-19 14:40:55 +0100

    This should make renewal work.

--- lib/Net/DomainRegistration/Simple/ResellerClub.pm
+++ lib/Net/DomainRegistration/Simple/ResellerClub.pm
@@ -1,4 +1,5 @@
 package Net::DomainRegistration::Simple::ResellerClub;
+our $testing = 0;
 use Data::Dumper;
 use Carp;
 use LWP::Simple;
@@ -42,22 +43,49 @@ sub _req {
     $u->query_form("auth-userid" => $self->{username}, 
                    "auth-password" => $self->{password},
                    %args);
+    if ($testing) { warn " > $u \n"; }
     my $res = LWP::Simple::get($u);
     return unless $res;
     $res = eval  { decode_json($res) };
+    if ($testing) { warn " < ".Dumper($res); }
     return $res;
 }
 
-sub register { return 1 }
+sub register { 
+    # We're going to use the same strategy as Nominet here, creating
+    # domain-specific contact records, although there's a separate step
+    # because we have to create a customer as well
+    my ($self, %args) = @_;
+    $self->_check_register(\%args);
+    return if !$self->is_available($args{domain});
+
+    my $id = sprintf("%x", time);
+    my %stuff = $self->_contact_set(%args) or return;
+    $self->{epp}->create_contact({ id => $id, %stuff, authInfo => "1234" }) or return
+
+    return 1 
+
+}
 sub is_available { 
     my ($self, $domain) = @_;
     $domain =~ /([^\.]+)\.(.*)/;
     my $res = $self->_req("domains/available", "domain-name" => $1, tlds => $2,
     "suggest-alternative" => "false");
-    die Dumper $res->{$domain};
+    $res->{$domain}{status} eq "available";
 }
 
-sub renew { return 1 }
+sub renew {
+    my ($self, %args) = @_;
+    $self->_check_renew(\%args);
+    my $id = $self->_req("domains/orderid", "domain-name" => $args{domain}) or return;
+    my $details = $self->_req("domains/details", "order-id" => $id,
+options => "OrderDetails") or return; 
+
+    $self->_req("domains/renew", "order-id" => $id, years => $args{years},
+                    "exp-date" => $details->{endtime}, 
+                    "invoice-option" => $args{invoice} || "NoInvoice"
+    );
+}
 sub revoke { return 1 }
 sub change_contact { return 1 }
 sub set_nameservers { return 1 }
